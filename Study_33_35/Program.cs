f1a8a94a6a65286a75bbeca0e0c9112ff7b9628d
혁준 권 <kkynks0@naver.com>
2023-04-25 21:36:21 +0900

    예제

--- Study_33_35/Program.cs
+++ Study_33_35/Program.cs
@@ -66,6 +66,7 @@ class FieldInitializer
 }
 */
 
+/*
 //[?] public과 private 액세스 한정자  
 using System;
 
@@ -94,5 +95,51 @@ class PublicPrivate
         Console.WriteLine(TestClass.siteName); // public 멤버는 외부에 노출
         // private 필드의 값을 외부에 공개하고자할 때에는 public으로 공개
         Console.WriteLine(TestClass.GetName());
+    }
+}
+*/
+
+using System;
+
+namespace ConstructorAll
+{
+    public class Person
+    {
+        private static readonly string _Name;
+        private int _Age;
+
+        //[!] 생성자 종류: 정적(static)과 인스턴스(instance)
+        //[1] 정적 생성자
+        static Person() { _Name = "홍길동"; }
+        //[2] 인스턴스 생성자: 매개변수가 없는 생성자
+        public Person() { _Age = 21; }
+        //[3] 인스턴스 생성자: 매개변수가 있는 생성자
+        public Person(int _Age)
+        {
+            this._Age = _Age; // this.필드 = 매개변수;
+        }
+
+        //[4] 정적 메서드
+        public static void Show()
+        {
+            Console.WriteLine("이름 : {0}", _Name);
+        }
+        //[5] 인스턴스 메서드
+        public void Print()
+        {
+            Console.WriteLine("나이 : {0}", _Age);
+        }
+    }
+    class ConstructorAll
+    {
+        static void Main()
+        {
+            //[A] 정적 생성자 실행
+            Person.Show(); // 정적인 멤버 호출
+
+            //[B] 인스턴스 생성자 실행
+            (new Person()).Print(); // 인스턴스 멤버 호출
+            (new Person(22)).Print();
+        }
     }
 }
